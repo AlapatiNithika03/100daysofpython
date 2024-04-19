726a91cd4362143684ced9bd450abdd88c29eec8
AlapatiNithika03 <165460228+AlapatiNithika03@users.noreply.github.com>
2024-04-19 15:00:46 +0530

    Update main.py

--- HangmanGame/main.py
+++ HangmanGame/main.py
@@ -22,6 +22,7 @@ while not end_of_game:
         display[position] = letter
   #if guess is not in the word
   if guess not in chosen_word:
+    print(f"You guessed {guess}, that's not in the word. You lose a life.")
     lives -= 1
     if lives==0:
       end_of_game=True
